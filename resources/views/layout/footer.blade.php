794e6a41458041e7ec1b9074082e85e130cc0fec
chidokato <tuan.pn92@gmail.com>
2024-12-10 16:22:00 +0700

    sdf

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,21 +4,21 @@
     <div class="container">
         <div class="row">
             <div class="col-md-5">
-                <h2>H&H Serviced Apartments <br> by Vietnam Homes Group</h2>
-                <p>Trải nghiệm của bạn tại H&H Serviced Apartments rất quý giá với chúng tôi.</p>
-                <p>Hãy chia sẻ những cảm nhận của bạn để chúng tôi có thể mang đến những trải nghiệm tuyệt vời hơn nữa trong những lần ghé thăm tiếp theo</p>
+                <h2>{{__('lang.hnh-9')}}</h2>
+                <p>{{__('lang.hnh-10')}}</p>
+                <p>{{__('lang.hnh-11')}}</p>
             </div>
             <div class="col-md-3">
                 
             </div>
             <div class="col-md-4">
-                <h2>Văn phòng đại diện</h2>
-                <p>Số 21 Cầu Cốc, phường Tây Mỗ, quận Nam Từ Liêm, Hà Nội</p>
+                <h2>{{__('lang.hnh-13')}}</h2>
+                <p>{{__('lang.hnh-14')}}</p>
                 <p>+(84) 916 442 096</p>
             </div>
         </div>
         <hr>
-        <p>Theo dõi chúng tôi qua</p>
+        <p>{{__('lang.hnh-12')}}</p>
         <p>Facebook, Instagram, TikTok</p>
     </div>
 </footer>
