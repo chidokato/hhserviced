067b0525c352511dcb4a8687d7e341cd00d0870e
chidokato <tuan.pn92@gmail.com>
2024-12-26 11:17:18 +0700

    sdf

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-md-4">
                 <h2>{{__('lang.hnh-9')}}</h2>
                 <p>{{__('lang.hnh-10')}}</p>
                 <p>{{__('lang.hnh-11')}}</p>
@@ -11,15 +11,23 @@
             <div class="col-md-3">
                 
             </div>
-            <div class="col-md-4">
+            <div class="col-md-5">
                 <h2>{{__('lang.hnh-13')}}</h2>
                 <p>{{__('lang.hnh-14')}}</p>
                 <p>+(84) 916 442 096</p>
             </div>
         </div>
         <hr>
-        <p>{{__('lang.hnh-12')}}</p>
-        <p>Facebook, Instagram, TikTok</p>
+        <div class="bottom">
+            <h3>{{__('lang.hnh-12')}}</h3>
+            <div class="ft-social">
+                <ul class="social">
+                    <li><a href="" class=""><i class="icon-facebook"></i></a></li>
+                    <li><a href="" class=""><i class="icon-youtube"></i></a></li>
+                    <li><a href="" class=""><i class="icon-zalo"></i></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>
 <!------------------- END: FOOTER ------------------->
